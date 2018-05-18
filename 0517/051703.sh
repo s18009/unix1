7fb349ff473f4bb35372d8094c074f757c656117
s18009 <s18009@std.it-college.ac.jp>
2018-05-18 05:29:26 +0000

    １７日の課題

---
+++ 0517/051703.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#touch dir1/dir2/dir3/file{01,,10}
+
+touch dir1/dir2/dir3/file01
+touch dir1/dir2/dir3/file02
+touch dir1/dir2/dir3/file03
+touch dir1/dir2/dir3/file04
+touch dir1/dir2/dir3/file05
+touch dir1/dir2/dir3/file06
+touch dir1/dir2/dir3/file07
+touch dir1/dir2/dir3/file08
+touch dir1/dir2/dir3/file09
+touch dir1/dir2/dir3/file10
---
+++ 0517/051704.sh
@@ -0,0 +1,11 @@
+#!bin/bash
+
+#rm dir1/dir2/dir3/file{01..05}
+rm dir1/dir2/dir3/file01
+rm dir1/dir2/dir3/file02
+rm dir1/dir2/dir3/file03
+rm dir1/dir2/dir3/file04
+rm dir1/dir2/dir3/file05
+
+
+
