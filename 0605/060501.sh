c7430addb62d4be7543ea7cef9a2881f5a782276
s18009 <s18009@std.it-college.ac.jp>
2018-06-06 02:10:23 +0000

    ディレクトリとファイルがわかるように修正

--- 0605/060501.sh
+++ 0605/060501.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-find ~ -type f | wc -l
-find ~ -type d | wc -l
+file="$(find ~ -type f | wc -l)"
+directory="$(find ~ -type d | wc -l)"
+
+echo "ディレクトリの検索結果 ($directory個)"
+echo "ファイルの検索結果 ($file個)"
