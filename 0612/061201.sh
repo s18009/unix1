de996dbf5860ed6ef2b6d81bc51490e719f9f401
s18009 <s18009@std.it-college.ac.jp>
2018-06-20 01:10:21 +0000

    １２日の課題修正

--- 0612/061201.sh
+++ 0612/061201.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-find ~ -type f | sed 's/\(,*\)/\"\1\"/' | xargs du -h | sort -nr | head -n 5 
+sudo find ~/ -type f | sed 's/\(.*\)/\"\1\"/g' | xargs du -b | sort -nr | head -n 5| awk '{pirnt $2}' | xargs du -h
